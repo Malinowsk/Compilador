8f7ecc8ddf1f82c3762aa72ca8e37708b803f170
Juan Ignacio Rago <juanrago2015@gmail.com>
2021-09-02 19:22:43 -0300

    Agrego clase Main.java , ejemplo de scaneo del archivo (cosas/AccionesSemanticas.txt)

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,8 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-<<<<<<< HEAD
         Scanner scannan = new Scanner(new File("./cosas/AccionesSemanticas.txt"));
-=======
-        Scanner scannan = new Scanner(new File("./AccionesSemanticas.txt"));
->>>>>>> 7a481be7edc4c14558fdc10d00b376d6e702b7cc
+
         ArrayList<String> lineas = new ArrayList<>();
         while(scannan.hasNext()){
             lineas.add(scannan.nextLine());
