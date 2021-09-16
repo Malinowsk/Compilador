664b608706565cfc1df6dfa80c3276468115c5c6
Juan Ignacio Rago <juanrago2015@gmail.com>
2021-09-16 18:00:54 -0300

    fecha 16/9

---
+++ src/AccionSemantica.java
@@ -0,0 +1,66 @@
+public class AccionSemantica {
+
+    public static void accion1(){
+
+    }
+
+    public static void accion2(){
+
+    }
+
+    public static void accion3(){
+
+    }
+
+    public static void accion4(){
+
+    }
+
+    public static void accion5(){
+
+    }
+
+
+    public static void accion6(){
+
+    }
+
+    public static void accion7(){
+
+    }
+
+    public static void accion8(){
+
+    }
+
+    public static void accion9(){
+
+    }
+
+    public static void accion10(){
+
+    }
+
+
+    public static void accion11(){
+
+    }
+
+    public static void accion12(){
+
+    }
+
+    public static void accion13(){
+
+    }
+
+    public static void accion14(){
+
+    }
+
+    public static void accion15(){
+
+    }
+
+
+}
